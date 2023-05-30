d99b1fe1c626a75ee41b4e855df33db5aac23397
Jiaying Li <lijiaying@pjlab.org.cn>
2023-05-30 11:12:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def greet(name):
     return a
 
 
+print("--------print env-----------")
+print("http_proxy: " + os.getenv('http_proxy'))
+print("https_proxy: " + os.getenv('https_proxy'))
+print("no_proxy: " + os.getenv('no_proxy'))
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 
 
