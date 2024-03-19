5843e17f973a561916c4a3f4b2fc4695ab9d068e
PJLAB\lijiaying <lijiaying@pjlab.org.cn>
2024-03-19 19:32:36 +0800

    streamlit

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
-from subprocess import Popen, PIPE, STDOUT
-
-
-def exe_shell(command: str, shell: str = '/bin/bash'):
-    return Popen(command, stdout=PIPE, stderr=STDOUT, shell=True, executable=shell)
+import streamlit as st
 
 
 if __name__ == '__main__':
-    process=exe_shell(command="echo 'start to run shell script' && bash run_streamlit.sh")
-    try:
-        with (process.stdout):
-            y = 0
-            for line in iter(process.stdout.readline, b''):
-                # s = str(line).replace("b'", "").replace("'", "").replace("\\n", "")
-                s = line.decode('utf-8')
-                print(s)
-    except Exception as e:
-        print(e)
+    st.write("# Hello World")
--- run_streamlit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-streamlit hello --server.address=0.0.0.0 --server.port=7860
